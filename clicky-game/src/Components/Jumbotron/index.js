7e37ba7359f808cc12ef7af7525c702f64956ad4
Julian Piron <pironj27@gmail.com>
2019-07-03 20:11:30 -0700

    added jumbotron component

---
+++ clicky-game/src/Components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Clicky Memory Game</h1>
+        <p class="lead">Click on the images below to earn points. If you click on the same image twice the game will end.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
